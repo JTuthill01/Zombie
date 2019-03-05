166a45544d194277268a0d9d7adb31ba39a806c6
JTuthill01 <45497162+JTuthill01@users.noreply.github.com>
2019-03-05 15:42:39 -0600

    Add files via upload

--- GameState.cpp
+++ GameState.cpp
@@ -20,8 +20,14 @@ void GameState::update(const float & deltaTime)
 	this->mPlayer.update(deltaTime);
 	this->updatePlayerInput(deltaTime);
 
-	if (this->mPlayer.getPosition().x > this->pWindow->getSize().x)
-		this->mLevels.emplace(new Level::Zombies(this->pWindow, &this->mLevels));
+	if (this->mPlayer.isLoadLevel() == false)
+	{
+		if (this->mPlayer.getPosition().x > this->pWindow->getSize().x)
+		{
+			this->mLevels.emplace(new Level::Zombies(this->pWindow, &this->mLevels));
+			this->mPlayer.setLoadLevel(true);
+		}
+	}
 
 	if (!this->mLevels.empty())
 		this->mLevels.top()->update(deltaTime);
@@ -45,10 +51,11 @@ void GameState::updatePlayerInput(const float & deltaTime)
 
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
 		this->mPlayer.move(-1.F, 0.F, deltaTime);
+
+	std::cout << this->mPlayer.pMovementComponent->getVelocity().x << " -  " << this->mPlayer.pMovementComponent->getVelocity().x << "\n";
 }
 
 void GameState::initLevel()
 {
 	this->mLevels.push(new Level::Orc(this->pWindow, &this->mLevels));
 }
-
